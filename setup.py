da9d50cc15fed5a77bd7bef1ebf55933ccc4870b
Kseniya <ksinnd@gmail.com>
2023-06-14 19:27:42 +0500

    add simple consumer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='microservices_communication',
+    name='services_communication',
     version='1.0.27',
     packages=find_packages(),
     url='https://github.com/ksinn/django-microservices-communication',
