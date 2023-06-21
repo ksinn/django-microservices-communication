2408150599a59b0da2d62dfd37910b1f25f4d4cb
Kseniya <ksinnd@gmail.com>
2023-06-21 20:16:33 +0500

    add decode datetime in event payload

--- services_communication/models.py
+++ services_communication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 class PublishedEventQueue(models.Model):
@@ -8,4 +9,5 @@ class PublishedEventQueue(models.Model):
     event_time = models.DateTimeField(auto_now_add=True)
     aggregate = models.CharField(max_length=64)
     event_type = models.CharField(max_length=64)
-    payload = models.JSONField()
+    payload = models.JSONField(encoder=DjangoJSONEncoder)
+
