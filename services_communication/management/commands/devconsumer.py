9147712a6476f0e3f47ef7106fcdf16c246b10fc
Kseniya <ksinnd@gmail.com>
2023-06-27 14:39:42 +0500

    add dev manage.py commands

---
+++ services_communication/management/commands/devconsumer.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+from services_communication.consumer.process import run_consumer
+
+import logging
+logging.basicConfig(level=logging.WARNING)
+from django.utils import autoreload
+
+
+class Command(BaseCommand):
+    help = "Run dev consumer"
+
+    def handle(self, *args, **options):
+            self.stdout.write(
+                self.style.SUCCESS('Starting DEVELOPMENT consumer')
+            )
+
+            try:
+                autoreload.run_with_reloader(run_consumer)
+            except KeyboardInterrupt:
+                self.style.SUCCESS('Consumer stopped')
+
+
