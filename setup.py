e2b0481e72d82be807f1e038a02756a53d464b29
Kseniya <ksinnd@gmail.com>
2023-06-30 19:18:51 +0500

    2.0.7: RestAPI exceptions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='services_communication',
-    version='2.0.6',
+    version='2.0.7',
     packages=find_packages(),
     url='https://github.com/ksinn/django-microservices-communication',
     author='ksinn',
