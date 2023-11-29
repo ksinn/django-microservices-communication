9f98d936b05a655cadac564177b963c0f59f0393
Kseniya <ksinnd@gmail.com>
2023-11-29 17:19:44 +0500

    make logger time rotated

--- services_communication/logging.py
+++ services_communication/logging.py
@@ -4,7 +4,21 @@ import os
 from django.conf import settings
 
 
-def get_logger(name, loggers={}):
+def get_logger(name="default", loggers={}, max_bytes=100*1000*1000, backup_count=3):
+    if name not in loggers:
+        handler = logging.handlers.RotatingFileHandler(os.path.join(settings.LOG_ROOT, '{}.log'.format(name)), maxBytes=max_bytes, backupCount=backup_count)
+        handler.suffix = "%Y%m%d"
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s: %(funcName)s - %(levelname)s - %(message)s'))
+
+        logger = logging.Logger(name, settings.APP_LOG_LEVEL)
+        logger.addHandler(handler)
+
+        loggers[name] = logger
+
+    return loggers[name]
+
+
+def get_logger(name, loggers={}, max_bytes=100*1000*1000, backup_count=3):
     if name not in loggers:
 
         level = getattr(settings, 'MICROSERVICES_COMMUNICATION_SETTINGS', {}).get('LOG_LEVEL')
@@ -16,7 +30,8 @@ def get_logger(name, loggers={}):
         LOG_ROOT = "../logs"
         if not os.path.exists(LOG_ROOT):
             os.makedirs(LOG_ROOT)
-        handler = logging.handlers.TimedRotatingFileHandler(os.path.join(LOG_ROOT, '{}.log'.format(name)), when="midnight", interval=1)
+        # handler = logging.handlers.TimedRotatingFileHandler(os.path.join(LOG_ROOT, '{}.log'.format(name)), when="midnight", interval=1)
+        handler = logging.handlers.RotatingFileHandler(os.path.join(settings.LOG_ROOT, '{}.log'.format(name)), maxBytes=max_bytes, backupCount=backup_count)
         handler.suffix = "%Y%m%d"
         handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s: %(funcName)s - %(levelname)s - %(message)s'))
         handler.setLevel(level)
