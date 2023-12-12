1d38f641a2130bd791cc95947e448b258af7e5f9
Kseniya <ksinnd@gmail.com>
2023-12-12 12:50:24 +0500

    edit setup

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='services_communication',
     version='2.1.2',
@@ -9,8 +12,9 @@ setup(
     url='https://github.com/ksinn/django-microservices-communication',
     author='ksinn',
     author_email='ksinnd@gmail.com',
-    description='Pub/Sub for service on django',
+    description='Pub/Sub for microservice on django',
     long_description_content_type="text/markdown",
+    long_description=long_description,
     install_requires=[
         "Django",
         "djangorestframework",
