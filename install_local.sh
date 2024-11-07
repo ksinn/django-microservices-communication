efdcfa9c189d92c65ff5e719530088a4c5fd1901
Kseniya <ksinnd@gmail.com>
2024-11-07 12:24:07 +0500

    sh

--- install_local.sh
+++ install_local.sh
@@ -1,3 +1,3 @@
 pip uninstall -y services_communication && python setup.py sdist && pip install dist/services_communication-*.tar.gz
 
-bdist_wheel
+rm -rf build/* && python setup.py sdist bdist_wheel
