e8ffaf7b43bbc2bc4891e87a055e5d897d6743fe
Kseniya <ksinnd@gmail.com>
2024-11-07 12:30:49 +0500

    add put and putch in rest helper

--- install_local.sh
+++ install_local.sh
@@ -1,3 +1,3 @@
 pip uninstall -y services_communication && python setup.py sdist && pip install dist/services_communication-*.tar.gz
 
-rm -rf build/* && python setup.py sdist bdist_wheel
+rm -rf build/* && rm -rf dist/* && python setup.py sdist bdist_wheel
