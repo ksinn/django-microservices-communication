5f2a54a04e7fa49e1543c761c4a0dfa998830ebd
Kseniya <ksinnd@gmail.com>
2025-10-22 14:23:33 +0500

    2.6.8 - Add not declaring options, fix bd connection for dns with reserved character

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    version='2.6.7',
+    version='2.6.8',
     name='django-microservices-communication',
     packages=find_packages(),
     url='https://github.com/ksinn/django-microservices-communication',
